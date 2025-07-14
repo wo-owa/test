5a890af61f87d9f5143199b20db28c4fd183c826
wo-o29 <woogur29@gmail.com>
2025-07-14 21:43:50 +0900

    0943

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+
 mkdir -p output
-cp -R ./test/* ./output
-cp -R ./output ./test/
+
+# .git, output, node_modules, .vscode 등은 제외하고 복사
+cp -R public src package.json pnpm-lock.yaml tsconfig.json webpack.config.js biome.json .gitignore output/
