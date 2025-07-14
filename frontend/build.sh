2902b7610b011dcf6920aca9361c2ff6a0c67da2
wo-o29 <woogur29@gmail.com>
2025-07-14 21:48:29 +0900

    0948

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+# output 폴더 비우기
+rm -rf output
 mkdir -p output
 
-# .github과 frontend 두 폴더 전체를 output 폴더로 복사
+# .github 복사
 cp -R ../.github ./output/
-cp -R . ./output/frontend/
+
+# rsync로 output 폴더 제외하고 frontend 전체 복사
+apk add --no-cache rsync
+rsync -av --exclude=output ./ ./output/frontend/
+rsync -av --exclude=output ./ ./output/backend/
